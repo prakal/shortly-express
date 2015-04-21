0fc93b6745c05bceac47f2d459d12c2b418a83f3
prakal <kalvapal@ualberta.ca>
2015-04-21 11:31:29 -0700

    managed to get all 14 tests to pass. Implemented login, logout, and sessions times. Managed to destroy session upon logout.

--- shortly.js
+++ shortly.js
@@ -2,6 +2,7 @@ var express = require('express');
 var util = require('./lib/utility');
 var partials = require('express-partials');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 
 
 var db = require('./app/config');
@@ -11,8 +12,11 @@ var Links = require('./app/collections/links');
 var Link = require('./app/models/link');
 var Click = require('./app/models/click');
 var crypto = require('crypto');
+var session = require('express-session');
 
 var app = express();
+app.use(cookieParser('Frank and Pranav sign'));
+app.use(session());
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -30,8 +34,17 @@ app.get('/', function(req, res) {
       // show index.html
     // else
       // render login page
-
-  res.redirect(302, "http://" + req.headers.host + "/login");
+    console.log("/ session", req.session);
+    console.log("/ session maxAge: ", req.session.cookie.maxAge);
+    if (req.session.cookie.maxAge > 0) {
+      // render index
+      res.render('index');
+    } else {
+      // if cookie is invalid,
+      // then redirect
+      res.redirect(302, "http://" + req.headers.host + "/login");
+    }
+  // res.redirect(302, "http://" + req.headers.host + "/login");
 });
 
 
@@ -43,12 +56,20 @@ function(req, res) {
 
 app.get('/links',
 function(req, res) {
-  console.log("links cookie", req.headers);
+  console.log("links session", req.session);
+  console.log("links session maxAge: ", req.session.cookie.maxAge);
+  if (req.session.cookie.maxAge > 0) {
+    // render links
+    console.log('get links: our cookie age is alright');
+    Links.reset().fetch().then(function(links) {
+      res.send(200, links.models);
+    });
+  } else {
+    // if cookie is invalid,
+    // then redirect
+    res.redirect(302, "http://" + req.headers.host + "/login");
 
-  res.redirect(302, "http://" + req.headers.host + "/login");
-   // Links.reset().fetch().then(function(links) {
-  //   res.send(200, links.models);
-  // });
+  }
 });
 
 app.post('/links',
@@ -90,6 +111,12 @@ function(req, res) {
 // Write your authentication routes here
 /************************************************************/
 
+app.get('/logout', function(req, res) {
+  req.session.destroy(function(){
+    res.redirect(302, '/login');
+  });
+});
+
 
 app.get('/login', function(req, res) {
   res.render('login');
@@ -101,10 +128,12 @@ app.post('/login', function(req, res) {
   qb.where({username: req.body.username}).select().then(function(resp) {
     console.log('login response: ', resp);
     Users.resetQuery();
-    if (resp[0].password === req.body.password) {
-      res.cookie("mycookieKey", "mycookieValue", {maxAge: 900000});
-      console.log('login cookie', res.headers);
-      res.redirect(302, "/links");
+    if (resp.length > 0 && resp[0].password === req.body.password) {
+      req.session.regenerate(function(){
+        req.session.user = req.body.username;
+        req.session.cookie.maxAge = 15*60*1000;
+        res.redirect('/');
+        });
     } else {
       res.redirect(302, "/login");
     }
